1a67602104597f6c35cd4de584ad62398b961234
Nicoagr11 <nicolasoctober@gmail.com>
2024-11-06 12:47:12 -0300

    menu meego ui

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,29 +2,49 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import { Card, Typography } from '@mui/material';
+import { Card, Typography, Stack, IconButton } from '@mui/material';
+import WifiIcon from '@mui/icons-material/Wifi';
+import BluetoothIcon from '@mui/icons-material/Bluetooth';
+import PersonIcon from '@mui/icons-material/Person';
+import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
 
 export default function Home() {
   return (
     <>
-      <Container maxWidth="sm" sx={{ bgcolor: 'black' }}>
-        {/* <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} /> */}
-        <Typography>Portfolio</Typography>
-        <Card sx={{ borderRadius: 5 }}>
-            <Typography>-Tecnologias</Typography>
+      <CssBaseline />
+      <Container maxWidth="sm" sx={{ bgcolor: 'black', padding: 2, borderRadius: 3 }}>
+        <Typography variant="h6" sx={{ color: 'white', mb: 2 }}>Menu</Typography>
 
+        <Card sx={{ display: 'flex', alignItems: 'center', mb: 1, bgcolor: '#333', borderRadius: 5, padding: 1 }}>
+          {/* <IconButton color="primary"><WifiIcon /></IconButton> */}
+          <Stack sx={{ ml: 1 }}>
+            <Typography sx={{ color: 'white' }}>Skills & technologies</Typography>
+            <Typography variant="body2" sx={{ color: 'gray' }}>Connected</Typography>
+          </Stack>
         </Card>
-        <Card sx={{ borderRadius: 5 }}>
-            <Typography>-Frameworks</Typography>
 
+        <Card sx={{ display: 'flex', alignItems: 'center', mb: 1, bgcolor: '#333', borderRadius: 5, padding: 1 }}>
+          {/* <IconButton color="primary"><BluetoothIcon /></IconButton> */}
+          <Stack sx={{ ml: 1 }}>
+            <Typography sx={{ color: 'white' }}>Projects</Typography>
+            <Typography variant="body2" sx={{ color: 'gray' }}>Off</Typography>
+          </Stack>
         </Card>
-        <Card sx={{ borderRadius: 5 }}>
-            <Typography>-Proyectos</Typography>
 
+        <Card sx={{ display: 'flex', alignItems: 'center', mb: 1, bgcolor: '#333', borderRadius: 5, padding: 1 }}>
+          <IconButton color="primary"><PersonIcon /></IconButton>
+          <Stack sx={{ ml: 1 }}>
+            <Typography sx={{ color: 'white' }}>CV</Typography>
+            <Typography variant="body2" sx={{ color: 'gray' }}>Offline</Typography>
+          </Stack>
         </Card>
-        <Card sx={{ borderRadius: 5 }}>
-            <Typography>-Contactame</Typography>
 
+        <Card sx={{ display: 'flex', alignItems: 'center', mb: 1, bgcolor: '#333', borderRadius: 5, padding: 1 }}>
+          <IconButton color="primary"><CloudDownloadIcon /></IconButton>
+          <Stack sx={{ ml: 1 }}>
+            <Typography sx={{ color: 'white' }}>Contact</Typography>
+            <Typography variant="body2" sx={{ color: 'gray' }}>1 ongoing transfer</Typography>
+          </Stack>
         </Card>
       </Container>
     </>
